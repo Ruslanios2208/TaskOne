2b4bbfdbaaa3509b7050062654c4053a26c503ea
Ruslanios22 <ilgamovich@gmail.com>
2023-03-07 12:28:56 +0800

    Done

--- TaskOne/ViewController.swift
+++ TaskOne/ViewController.swift
@@ -9,23 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var squareView: UIView!
-    
-    private let shadowView = ShadowView()
+    lazy var cyanView = View()
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        
+        cyanView.layer.cornerRadius = 14
+        cyanView.backgroundColor = .systemCyan
+        view.addSubview(cyanView)
     }
     
-    private func setup() {
-        squareView.addSubview(shadowView)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        shadowView.translatesAutoresizingMaskIntoConstraints = false
+        cyanView.frame = .init(
+            x: 100,
+            y: cyanView.frame.origin.y,
+            width: 120,
+            height: 120
+        )
+        cyanView.center.y = view.frame.height / 2
+    }
+}
+
+class View: UIView {
+    
+    let gradientLayer = CAGradientLayer()
+    
+    init() {
+        super.init(frame: .zero)
         
-        shadowView.leadingAnchor.constraint(equalTo: squareView.leadingAnchor).isActive = true
-        shadowView.trailingAnchor.constraint(equalTo: squareView.trailingAnchor).isActive = true
-        shadowView.topAnchor.constraint(equalTo: squareView.topAnchor).isActive = true
-        shadowView.bottomAnchor.constraint(equalTo: squareView.bottomAnchor).isActive = true
+        backgroundColor = .systemCyan
+        layer.cornerCurve = .continuous
+        
+        layer.shadowOpacity = 0.3
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .init(width: .zero, height: 12)
+        layer.shadowRadius = 12
+        
+        gradientLayer.startPoint = .init(x: 0, y: 0)
+        gradientLayer.endPoint = .init(x: 1, y: 0.5)
+        gradientLayer.colors = [
+            UIColor.systemRed.withAlphaComponent(0.1),
+            UIColor.systemRed.cgColor
+        ]
+        layer.addSublayer(gradientLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        if layer == self.layer {
+            gradientLayer.frame = bounds
+            gradientLayer.cornerRadius = layer.cornerRadius
+        }
     }
 }
